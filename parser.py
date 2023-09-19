6c1f998c6f71b024e83c8039d50cebaf39a33e79
Denis0987 <Denis8.9@bk.ru>
2023-09-19 14:14:00 +0600

    Добавил неявку по уважительной

--- parser.py
+++ parser.py
@@ -8,12 +8,16 @@ from secrets import cookies
 def checkPresent():
     URL = "https://up.omgtu.ru/index.php?r=journal/index"
 
-    with open("present_names.json", encoding='utf-8') as f:
+    with open("present_names.json", 'r', encoding='utf-8') as f:
         # people = [name.upper() for name in json.load(f)]
         people = json.load(f)
 
     values = dict()
+    arr_selected_value = []
 
+    for count_value in people:
+        arr_selected_value.append(people[count_value])
+    print(arr_selected_value)
     with requests.Session() as s:
         r = s.get(URL, cookies=cookies)
 
@@ -28,12 +32,16 @@ def checkPresent():
         for row in table_rows:
             name = row.th.get_text().upper()
             for person in people:
-                if person == name:
+                if person.upper() == name:
                     columns = row.find_all("td")
                     for col in columns:
                         if bool(col.find_all()):
-                            val = col.find("select")["name"]
-                            values[val] = 1
+                            if(people[person] == 1):
+                                val = col.find("select")["name"]
+                                values[val] = 1
+                            elif(people[person] == 4):
+                                val = col.find("select")["name"]
+                                values[val] = 2
 
     r = requests.post(URL, data=values, cookies=cookies)
     print(r)
--- secrets.py
+++ secrets.py
@@ -1,4 +1,4 @@
 TOKEN = "5932776750:AAFvp-zgb04h2SZ6zlOjKhhaJjJr3B4bHT4"
 cookies = {
-    "STUDSESSID": "or6ifaa7dmsb5luntsg37bq1q1"
+    "STUDSESSID": "snmd46d7j5c9ojq0vocfe54fl2"
 }
